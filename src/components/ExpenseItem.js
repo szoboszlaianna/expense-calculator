c4b2f5ef1fd13ee663c427d74aeb72247e2d4006
szoboszlaianna <szoboszlai.anna0@gmail.com>
2021-10-28 14:57:16 +0200

    Expenses

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import Card from './Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = props => {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
+  return (
+    <Card className='expense-item'>
+      <div className='expense-date'>
+        <div className='expense-month'>{month}</div>
+        <div className='expense-year'>{year}</div>
+        <div className='expense-day'>{day}</div>
+      </div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
